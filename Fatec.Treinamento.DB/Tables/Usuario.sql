255091443e9ae19cd96558c3dbe6b1b0f60aba81
fbarbieri <fbarbieri@live.com>
2017-01-23 23:39:37 -0200

    Scripts de carga inicial e publish

--- Fatec.Treinamento.DB/Tables/Usuario.sql
+++ Fatec.Treinamento.DB/Tables/Usuario.sql
@@ -2,7 +2,7 @@
     [Id]    INT           IDENTITY (1, 1) NOT NULL,
     [Nome]  VARCHAR (100) NOT NULL,
     [Email] VARCHAR (100) NOT NULL,
-    [Senha] VARCHAR (20)  NOT NULL,
+    [Senha] VARCHAR (100)  NOT NULL,
     [Ativo] BIT           CONSTRAINT [DF_Usuario_Ativo] DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_Usuario] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
