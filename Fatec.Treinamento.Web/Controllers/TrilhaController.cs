df9f9c597a381867fcb04a41419cb6ded3434d22
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-01-28 04:28:41 -0200

    Add the controller 'TrilhaController' to Fatec.Treinamento.Web/Controllers/ folder

---
+++ Fatec.Treinamento.Web/Controllers/TrilhaController.cs
@@ -0,0 +1,49 @@
+﻿using Fatec.Treinamento.Data.Repositories;
+using Fatec.Treinamento.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fatec.Treinamento.Web.Controllers
+{
+    public class TrilhaController : Controller
+    {
+        // GET: Trilha
+        public ActionResult Index()
+        {
+            var repo = new TrilhaRepository();
+            var lista = repo.Listar();
+
+            return View(lista);
+        }
+
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Criar(Trilha trilha)
+        {
+            using (var repo = new TrilhaRepository())
+            {
+                var inserido = repo.Inserir(trilha);
+
+                if (inserido.Id == 0)
+                {
+                    ModelState.AddModelError("", "Erro");
+                }
+            }
+
+            
+
+            
+
+            return RedirectToAction("Index");
+        }
+    }
+
+
+}
