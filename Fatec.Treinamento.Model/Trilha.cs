901d631aef4c92238081d17a3db595184439a59d
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-01-28 04:27:30 -0200

    Add the model 'Trilha' to Fatec.Treinamento.Model/ folder

---
+++ Fatec.Treinamento.Model/Trilha.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatec.Treinamento.Model
+{
+    public class Trilha
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Ativa { get; set; }
+    }
+}
