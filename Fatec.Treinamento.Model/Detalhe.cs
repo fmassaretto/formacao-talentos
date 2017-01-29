8b14e5c56dbd4f0dd38245e4fcaf94bc240b4f7c
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-01-29 17:55:10 -0200

    Created model 'Detalhe' and the properties 'TotalCapitulo' and 'Pontos'

---
+++ Fatec.Treinamento.Model/Detalhe.cs
@@ -0,0 +1,15 @@
+﻿using Fatec.Treinamento.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatec.Treinamento.Model
+{
+    public class Detalhe : DetalhesCurso
+    {
+        public int TotalCapitulos { get; set; }
+        public int Pontos { get; set; }
+    }
+}
