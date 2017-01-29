44ea83b2907e1dd3e97c3beca0b7ff9fb1ce4535
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-01-29 17:42:53 -0200

    Add method 'DetalheCurso'

--- Fatec.Treinamento.Data/Repositories/CursoRepository.cs
+++ Fatec.Treinamento.Data/Repositories/CursoRepository.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Fatec.Treinamento.Model.DTO;
 using Fatec.Treinamento.Data.Repositories.Base;
 using Dapper;
+using Fatec.Treinamento.Model;
 
 namespace Fatec.Treinamento.Data.Repositories
 {
@@ -28,6 +29,43 @@ namespace Fatec.Treinamento.Data.Repositories
                  WHERE c.nome like @Nome
                  ORDER BY c.Nome",
               param: new { Nome = "%" + nome + "%" }
+            ).ToList();
+        }
+
+        public IEnumerable<DetalhesCurso> ListarTodosCursos()
+        {
+            return Connection.Query<DetalhesCurso>(
+              @"SELECT
+	                c.Id,
+	                c.Nome,
+	                a.Nome as Assunto,
+	                u.Nome as Autor,
+	                c.DataCriacao,
+	                c.Classificacao
+                 FROM curso c
+                 inner join assunto a on c.IdAssunto = a.id
+                 inner join usuario u on c.IdAutor = u.Id
+                 ORDER BY c.Nome"
+            ).ToList();
+        }
+
+        public IEnumerable<Detalhe> DetalheCurso(int id)
+        {
+            return Connection.Query<Detalhe>(
+              @"SELECT
+	                c.Id,
+	                c.Nome,
+	                a.Nome as Assunto,
+	                u.Nome as Autor,
+	                c.DataCriacao,
+	                c.Classificacao,
+                    cd.Descricao
+                 FROM curso c
+                 inner join assunto a on c.IdAssunto = a.id
+                 inner join usuario u on c.IdAutor = u.Id
+                 inner join curso_descricao cd on cd.IdCurso = c.Id
+                 WHERE c.Id = @Id",
+              param: new { Id = id }
             ).ToList();
         }
     }
