ca849484cf6ea73b705f3c28a949ccb047554425
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-01-30 08:21:39 -0200

    Added new controller

---
+++ Fatec.Treinamento.Web/Controllers/VoltarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fatec.Treinamento.Web.Controllers
+{
+    public class VoltarController : Controller
+    {
+        //// GET: Voltar
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult ActionDeVoltar(string returnUrl)
+        {
+            if (returnUrl != "")
+                return Redirect(returnUrl);
+
+            return null;
+        }
+    }
+}
