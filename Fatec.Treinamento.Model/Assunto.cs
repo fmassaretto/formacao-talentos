523ac068f2b2295013d385bcc59bc3560c0bb9cd
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-01-30 08:18:32 -0200

    Add inheit class 'AssuntoCursoUsuario'

--- Fatec.Treinamento.Model/Assunto.cs
+++ Fatec.Treinamento.Model/Assunto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fatec.Treinamento.Model.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Fatec.Treinamento.Model
 {
-    public class Assunto
+    public class Assunto : AssuntoCursoUsuario
     {
         public int Id { get; set; }
         public string Nome { get; set; }
