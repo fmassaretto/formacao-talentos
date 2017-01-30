a5935140b763c0f3936d142344ff5f6e1578c078
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-01-30 08:21:20 -0200

    Added new models

---
+++ Fatec.Treinamento.Model/DTO/AssuntoCursoUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatec.Treinamento.Model.DTO
+{
+    public class AssuntoCursoUsuario
+    {
+
+        public int IdCurso { get; set; }
+        public int IdAutor { get; set; }
+        public int IdAssunto { get; set; }
+
+        public string NomeCurso { get; set; }
+        public string NomeAssunto { get; set; }
+        public string NomeAutor { get; set; }
+
+        public string Assunto { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+
+        public int Classificacao { get; set; }
+    }
+}
---
+++ Fatec.Treinamento.Web/Models/AssuntoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Fatec.Treinamento.Model;
+using Fatec.Treinamento.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fatec.Treinamento.Web.Models
+{
+    public class AssuntoViewModel
+    {
+        public IEnumerable<Assunto> Assunto { get; set; }
+        public Assunto Obter { get; set; }
+    }
+}
