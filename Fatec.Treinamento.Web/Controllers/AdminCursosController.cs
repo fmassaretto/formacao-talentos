10a0f92944aab3e922476e429915a1feb92d56c2
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-02-01 08:51:08 -0200

    Tether plugin

---
+++ Fatec.Treinamento.Web/Controllers/AdminCursosController.cs
@@ -0,0 +1,41 @@
+﻿using Fatec.Treinamento.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fatec.Treinamento.Web.Controllers
+{
+    public class AdminCursosController : Controller
+    {
+        // GET: AdminCursos
+        [HttpGet]
+        public ActionResult Index()
+        {
+            using (var repo = new CursoRepository())
+            {
+                var lista = repo.ListarTodosCursos();
+                return View(lista);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Obter(int id)
+        {
+            using (var repo = new CursoRepository())
+            {
+                var lista = repo.Obter(id);
+                return View(lista);
+            }          
+        }
+
+        [HttpGet]
+        public ActionResult Criar()
+        {
+            return View();
+        }
+
+       
+    }
+}
