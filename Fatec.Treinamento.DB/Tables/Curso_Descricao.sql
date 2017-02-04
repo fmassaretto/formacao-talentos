fda122d2b26486b3b512afea2d188f84e30d757d
FabioNote-PC\FabioNote <fmassaretto@gmail.com>
2017-02-04 00:21:36 -0200

    New pages, images and data

---
+++ Fatec.Treinamento.DB/Tables/Curso_Descricao.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Curso_Descricao]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Descricao] VARCHAR(500) NULL, 
+    [IdCurso] INT NOT NULL, 
+    CONSTRAINT [FK_Curso_Descricao_Curso] FOREIGN KEY (IdCurso) REFERENCES [Curso]([Id])
+)
