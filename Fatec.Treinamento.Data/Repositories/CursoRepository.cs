8788b658045f63cdf6df59292aa5d3d0591caf83
fmassaretto <fabio_no_more@yahoo.com.br>
2017-02-05 20:13:27 -0200

    Minor format changes

--- Fatec.Treinamento.Data/Repositories/CursoRepository.cs
+++ Fatec.Treinamento.Data/Repositories/CursoRepository.cs
@@ -11,131 +11,131 @@ using Fatec.Treinamento.Model;
 
 namespace Fatec.Treinamento.Data.Repositories
 {
-    public class CursoRepository : RepositoryBase, ICursoRepository
-    {
-        public IEnumerable<AssuntoCursoUsuario> ListarCursosPorNome(string nome)
-        {
-            return Connection.Query<AssuntoCursoUsuario>(
-              @"SELECT
-	                c.Id,
-	                c.Nome,
-                    a.Id AS IdAssunto,
-                    u.Id AS IdAutor,
-	                a.Nome as NomeAssunto,
-	                u.Nome as NomeAutor,
-	                c.DataCriacao,
-	                c.Classificacao
-                 FROM curso c
-                 inner join assunto a on c.IdAssunto = a.id
-                 inner join usuario u on c.IdAutor = u.Id
-                 WHERE c.nome like @Nome
-                 ORDER BY c.Nome",
-              param: new { Nome = "%" + nome + "%" }
-            ).ToList();
-        }
+	public class CursoRepository : RepositoryBase, ICursoRepository
+	{
+		public IEnumerable<AssuntoCursoUsuario> ListarCursosPorNome(string nome)
+		{
+			return Connection.Query<AssuntoCursoUsuario>(
+			  @"SELECT
+					c.Id,
+					c.Nome,
+					a.Id AS IdAssunto,
+					u.Id AS IdAutor,
+					a.Nome as NomeAssunto,
+					u.Nome as NomeAutor,
+					c.DataCriacao,
+					c.Classificacao
+				 FROM curso c
+				 inner join assunto a on c.IdAssunto = a.id
+				 inner join usuario u on c.IdAutor = u.Id
+				 WHERE c.nome like @Nome
+				 ORDER BY c.Nome",
+			  param: new { Nome = "%" + nome + "%" }
+			).ToList();
+		}
 
-        public AssuntoCursoUsuario Obter(int id)
-        {
-            return Connection.Query<AssuntoCursoUsuario>(
-               @"SELECT c.Nome,
-               c.Classificacao,
-               a.Id AS IdAssunto,
-               u.Id AS IdAutor,
-               u.Nome AS NomeAutor,
-               a.Nome AS NomeAssunto
-               FROM Curso c
-               INNER JOIN Usuario u ON c.IdAutor = u.Id
-               INNER JOIN Assunto a ON c.IdAssunto = a.Id
-               WHERE c.Id = @Id",
-               param: new { Id = id }
-           ).FirstOrDefault();
-        }
+		public AssuntoCursoUsuario Obter(int id)
+		{
+			return Connection.Query<AssuntoCursoUsuario>(
+			   @"SELECT c.Nome,
+			   c.Classificacao,
+			   a.Id AS IdAssunto,
+			   u.Id AS IdAutor,
+			   u.Nome AS NomeAutor,
+			   a.Nome AS NomeAssunto
+			   FROM Curso c
+			   INNER JOIN Usuario u ON c.IdAutor = u.Id
+			   INNER JOIN Assunto a ON c.IdAssunto = a.Id
+			   WHERE c.Id = @Id",
+			   param: new { Id = id }
+		   ).FirstOrDefault();
+		}
 
-        public AssuntoCursoUsuario Atualizar(AssuntoCursoUsuario acu)
-        {
-            Connection.Execute(
-               @"BEGIN TRANSACTION;
+		public AssuntoCursoUsuario Atualizar(AssuntoCursoUsuario acu)
+		{
+			Connection.Execute(
+			   @"BEGIN TRANSACTION;
 
-                UPDATE Curso SET Curso.Nome = @Nome, Curso.Classificacao = @Classificacao
-	                FROM Curso c INNER JOIN Usuario u
-	                ON c.IdAutor = u.Id 
-	                And c.Id = @Id;
+				UPDATE Curso SET Curso.Nome = @Nome, Curso.Classificacao = @Classificacao
+					FROM Curso c INNER JOIN Usuario u
+					ON c.IdAutor = u.Id 
+					And c.Id = @Id;
 
-                UPDATE Assunto SET Nome = @NomeAssunto
-	                FROM Assunto a INNER JOIN Curso c
-	                ON c.IdAssunto = a.Id
-	                AND a.Id = @IdAssunto;
+				UPDATE Assunto SET Nome = @NomeAssunto
+					FROM Assunto a INNER JOIN Curso c
+					ON c.IdAssunto = a.Id
+					AND a.Id = @IdAssunto;
 
-                COMMIT;",
-               param: new
-               {
-                   acu.Nome,
-                   acu.Classificacao,
-                   acu.Id,
-                   acu.NomeAssunto,
-                   acu.IdAssunto
-               }
-            );
+				COMMIT;",
+			   param: new
+			   {
+				   acu.Nome,
+				   acu.Classificacao,
+				   acu.Id,
+				   acu.NomeAssunto,
+				   acu.IdAssunto
+			   }
+			);
 
-            return acu;
-        }
+			return acu;
+		}
 
-        public IEnumerable<AssuntoCursoUsuario> ListarTodosCursos()
-        {
-            return Connection.Query<AssuntoCursoUsuario>(
-              @"SELECT
-	                c.Id,
-	                c.Nome,
-                    a.Id AS IdAssunto,
-	                a.Nome AS NomeAssunto,
-                    u.Id AS IdAutor,
-	                u.Nome AS NomeAutor,
-	                c.DataCriacao,
-	                c.Classificacao
-                 FROM curso c
-                 inner join assunto a on c.IdAssunto = a.id
-                 inner join usuario u on c.IdAutor = u.Id
-                 ORDER BY c.Nome"
-            ).ToList();
-        }
+		public IEnumerable<AssuntoCursoUsuario> ListarTodosCursos()
+		{
+			return Connection.Query<AssuntoCursoUsuario>(
+			  @"SELECT
+					c.Id,
+					c.Nome,
+					a.Id AS IdAssunto,
+					a.Nome AS NomeAssunto,
+					u.Id AS IdAutor,
+					u.Nome AS NomeAutor,
+					c.DataCriacao,
+					c.Classificacao
+				 FROM curso c
+				 inner join assunto a on c.IdAssunto = a.id
+				 inner join usuario u on c.IdAutor = u.Id
+				 ORDER BY c.Nome"
+			).ToList();
+		}
 
-        public IEnumerable<Detalhe> DetalheCurso(int? id)
-        {
-            return Connection.Query<Detalhe>(
-              @"SELECT
-	                c.Id,
-	                c.Nome,
-                    a.Id AS IdAssunto,
-	                a.Nome as NomeAssunto,
-                    u.Id AS IdAutor,
-	                u.Nome as NomeAutor,
-	                c.DataCriacao,
-	                c.Classificacao,
-                    cd.Descricao AS Descricao
-                 FROM curso c
-                 inner join assunto a on c.IdAssunto = a.id
-                 inner join usuario u on c.IdAutor = u.Id
-                 inner join curso_descricao cd on cd.IdCurso = c.Id
-                 WHERE c.Id = @Id",
-              param: new { Id = id }
-            ).ToList();
-        }
+		public IEnumerable<Detalhe> DetalheCurso(int? id)
+		{
+			return Connection.Query<Detalhe>(
+			  @"SELECT
+					c.Id,
+					c.Nome,
+					a.Id AS IdAssunto,
+					a.Nome as NomeAssunto,
+					u.Id AS IdAutor,
+					u.Nome as NomeAutor,
+					c.DataCriacao,
+					c.Classificacao,
+					cd.Descricao AS Descricao
+				 FROM curso c
+				 inner join assunto a on c.IdAssunto = a.id
+				 inner join usuario u on c.IdAutor = u.Id
+				 inner join curso_descricao cd on cd.IdCurso = c.Id
+				 WHERE c.Id = @Id",
+			  param: new { Id = id }
+			).ToList();
+		}
 
-        public IEnumerable<DetalhesCurso> ListarCursosDetalhes()
-        {
-            return Connection.Query<DetalhesCurso>(
-              @"SELECT
-	                c.Id,
-	                c.Nome,
-	                a.Nome as Assunto,
-	                u.Nome as Autor,
-	                c.DataCriacao,
-	                c.Classificacao
-                 FROM curso c
-                 inner join assunto a on c.IdAssunto = a.id
-                 inner join usuario u on c.IdAutor = u.Id
-                 ORDER BY c.Nome"
-            ).ToList();
-        }
-    }
+		public IEnumerable<DetalhesCurso> ListarCursosDetalhes()
+		{
+			return Connection.Query<DetalhesCurso>(
+			  @"SELECT
+					c.Id,
+					c.Nome,
+					a.Nome as Assunto,
+					u.Nome as Autor,
+					c.DataCriacao,
+					c.Classificacao
+				 FROM curso c
+				 inner join assunto a on c.IdAssunto = a.id
+				 inner join usuario u on c.IdAutor = u.Id
+				 ORDER BY c.Nome"
+			).ToList();
+		}
+	}
 }
