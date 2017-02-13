9eb156c98c02d71a863fd5f9bd2258d49408e9e3
Fabio-Note\Fabio <fabio_no_more@yahoo.com.br>
2017-02-13 08:07:46 -0200

    Tests

--- Fatec.Treinamento.Web/Controllers/PainelController.cs
+++ Fatec.Treinamento.Web/Controllers/PainelController.cs
@@ -1,5 +1,8 @@
-﻿using Fatec.Treinamento.Data.Repositories;
+﻿using Fatec.Treinamento.Data;
+using Fatec.Treinamento.Data.Repositories;
+using Fatec.Treinamento.Model;
 using Fatec.Treinamento.Model.DTO;
+using Fatec.Treinamento.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,13 +16,41 @@ namespace Fatec.Treinamento.Web.Controllers
         // GET: Painel
         public ActionResult Index()
         {
-            IEnumerable<AssuntoCursoUsuario> listaTodos = new List<AssuntoCursoUsuario>();
+            //IEnumerable<AssuntoCursoUsuario> listaTodos = new List<AssuntoCursoUsuario>();
+            UsuarioPainelViewModel listar = new UsuarioPainelViewModel();
+
+            int idUsuario = 0;
+            using (UsuarioRepository usuario = new UsuarioRepository())
+            {
+                var listaUsuario = usuario.ListarPorNome(User.Identity.Name);
+
+                foreach (var item in listaUsuario)
+                {
+                    idUsuario = item.Id;
+                }
+            }
 
             using (CursoRepository repoTodos = new CursoRepository())
             {
-                listaTodos = repoTodos.ListarTodosCursos();
+                listar.Acu = repoTodos.ListarTodosCursos();
             }
-            return View(listaTodos);
+
+            using (TreinamentoRepository repoTrein = new TreinamentoRepository())
+            {
+                try
+                {
+                    listar.PontosUsuario = repoTrein.PontosUsuario(idUsuario);
+
+                }
+                catch (Exception)
+                {
+                    listar.PontosUsuario = 0;
+                   
+                }
+     
+            }
+
+            return View(listar);
         }
     }
 }
---
+++ Fatec.Treinamento.Web/Controllers/YouTubeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fatec.Treinamento.Web.Controllers
+{
+    public class YouTubeController : Controller
+    {
+        // GET: YouTube
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string RecuperarThumb(string codigoVideo)
+        {
+            //string[] result = enderecoVideo.Split('=');
+            //string codEmbed = result[1];
+            //return string.Format("http://img.youtube.com/vi/{0}/default.jpg", codEmbed);
+            return string.Format("http://img.youtube.com/vi/{0}/default.jpg", codigoVideo);
+        }
+
+        private string RecuperarEmbedVideo(string enderecoVideo)
+        {
+            string[] result = enderecoVideo.Split('=');
+            return result[1];
+        }
+    }
+}
