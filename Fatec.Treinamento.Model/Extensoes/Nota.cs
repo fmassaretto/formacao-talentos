228cc4e9725cba22903fdfd322d1f15727c42719
Fabio-Note\Fabio <fabio_no_more@yahoo.com.br>
2017-02-13 08:14:24 -0200

    Test

---
+++ Fatec.Treinamento.Model/Extensoes/Nota.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatec.Treinamento.Model.Enum
+{
+    public class Nota
+    {
+        public int Value { get; set; }
+        public string Name { get; set; }
+    }
+}
