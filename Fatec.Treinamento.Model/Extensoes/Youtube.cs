54b1a17e1e53be6cf6b9fdb01490298d90afb74d
Fabio-Note\Fabio <fabio_no_more@yahoo.com.br>
2017-02-13 08:05:50 -0200

    Tests

---
+++ Fatec.Treinamento.Model/Extensoes/Youtube.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatec.Treinamento.Model.Extensoes
+{
+    public class Youtube
+    {
+        public string RecuperarThumb(string codigoVideo)
+        {
+            return string.Format("http://img.youtube.com/vi/{0}/default.jpg", codigoVideo);
+        }
+    }
+}
---
+++ Fatec.Treinamento.Model/YouTubeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatec.Treinamento.Model
+{
+    public class YouTubeModel
+    {
+        public string EndImagem { get; set; }
+        public string EndVideo { get; set; }
+    }
+}
