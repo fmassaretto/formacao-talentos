a41b1b222309906c62ef0c5162f29e183a4b5321
Fabio-Note\Fabio <fabio_no_more@yahoo.com.br>
2017-02-13 08:13:42 -0200

    ViewModel for 'Painel'

---
+++ Fatec.Treinamento.Web/Models/UsuarioPainelViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Fatec.Treinamento.Model;
+using Fatec.Treinamento.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fatec.Treinamento.Web.Models
+{
+    public class UsuarioPainelViewModel
+    {
+        public IEnumerable<AssuntoCursoUsuario> Acu { get; set; }
+        public int PontosUsuario { get; set; }
+        public IEnumerable<TreinamentoCurso> CursosFinalizado { get; set; }
+        public IEnumerable<TreinamentoCurso> CursosAndamento { get; set; }
+        public IEnumerable<TreinamentoCurso> CursosFavoritos { get; set; }
+    }
+}
