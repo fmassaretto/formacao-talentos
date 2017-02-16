1efcc7e8dd9749700e07dd488cbbfcebc0c598cd
Fabio-Note\Fabio <fabio_no_more@yahoo.com.br>
2017-02-15 23:32:20 -0200

    Created 'Saiba mais' pages and some css adjustments

---
+++ Fatec.Treinamento.Web/Controllers/HelpController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fatec.Treinamento.Web.Controllers
+{
+    public class HelpController : Controller
+    {
+        // GET: Help
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Trilhas()
+        {
+            return View();
+        }
+
+        public ActionResult Autores()
+        {
+            return View();
+        }
+
+        public ActionResult Pontos()
+        {
+            return View();
+        }
+
+
+    }
+}
